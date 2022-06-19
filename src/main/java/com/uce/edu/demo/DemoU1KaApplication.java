152b3f9748f005651a58f2a3118a1a679cfa34ed
Xavier Aguilar <kevinjkevps4@gmail.com>
2022-06-19 16:36:08 -0500

    demo para crear todo

---
+++ src/main/java/com/uce/edu/demo/DemoU1KaApplication.java
@@ -0,0 +1,15 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoU1KaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoU1KaApplication.class, args);
+		
+		System.out.println("Prueba");
+	}
+
+}
